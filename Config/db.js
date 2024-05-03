3dfbf67e89a69940569284c7a2b2beb02cd33417
Ace10079 <minions10karan@gmail.com>
2024-05-03 12:06:08 +0530

    removing multer

--- Config/db.js
+++ Config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
-const connection = mongoose.createConnection(`mongodb://127.0.0.1:27017/Backend`).on('open', () => {
+const connection = mongoose.createConnection(`mongodb+srv://karan:karan@cluster0.xi3owp9.mongodb.net/Backend?retryWrites=true&w=majority&appName=Cluster0
+`).on('open', () => {
     console.log("MongoDB Connected");
 }).on('error', () => {
     console.log("MongoDB Connection error");
