9204e78d804028d16262241f043cd497ff8a5b23
Ace10079 <minions10karan@gmail.com>
2024-05-03 11:57:58 +0530

    removing multer

--- Routes/CustomerRouter.js
+++ Routes/CustomerRouter.js
@@ -2,24 +2,12 @@ const express = require('express');
 const router = express.Router();
 const UserController = require('../Controllers/CustomerController');
 const AdminController=require('../Controllers/AdminController')
-const multer = require('multer');
-const path = require("path");
-const ImageController=require('../Controllers/ImageController')
 
 
-const storage = multer.diskStorage({
-    destination: 'img',
-    filename: (req, file, cb) => {
-        cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
-    },
-});
-const upload = multer({ storage: storage }).single('img');
-router.post('/customer',upload, UserController.registerUser);
-router.post('/imageupload',upload, ImageController.registerImage);
+
+router.post('/customer',UserController.registerUser);
 router.post('/admin', AdminController.registerAdmin);
 router.put('/update', UserController.updateUser);
-router.put('/updateimg', UserController.updateImgUser);
-router.put('/updateimg', UserController.updateImgUser);
 router.get('/getuserbyemail', UserController.getUserByEmail);
 router.get('/getall', UserController.getAllUsers);
 router.put('/adminupdate', AdminController.updateAdmin);
